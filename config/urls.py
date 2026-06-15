058793e7916d223b4e0427f7c6bf756cd6ba28a9
Rojin4455 <rojinsaji35@gmail.com>
2026-06-16 01:43:19 +0530

    admin route changed

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from apps.authentication.views import ExchangeLogidView
 from apps.ghl.views import GhlRefreshHookView, GhlWebhookView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
     path("api/auth/", include("apps.authentication.urls")),
     path("api/ghl/", include("apps.ghl.urls")),
     path("api/db/", include("apps.dbapi.urls")),
